77b41a63268a3eeb0d71504757afaffba4f1b90d
p164551 <p164551@localdomain>
2014-04-09 11:23:33 +0200

    Modified the default Realurl-configuration: Parameter tx_podcast_display[controller]=Podcast will be mapped to podcast defaultToHTMLsuffixOnPrev removed

--- Configuration/RealURL/default.php
+++ Configuration/RealURL/default.php
@@ -1,32 +1,39 @@
 <?php
 
 $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets']['_DEFAULT']['podcast'] = array(
-	array(
-		'GETvar' => 'tx_podcast_display[action]',
-	),
-	array(
-		'GETvar' => 'tx_podcast_display[podcast]',
-		'lookUpTable' => array(
-			 'table' => 'tx_podcast_domain_model_podcast',
-			 'id_field' => 'uid',
-			 'alias_field' => 'title',
-			 'addWhereClause' => ' AND NOT deleted AND NOT hidden',
-			 'useUniqueCache' => 1,
-			 'useUniqueCache_conf' => array(
-				 'strtolower' => 1,
-				 'spaceCharacter' => '-',
-			 ),
-			 'enable404forInvalidAlias' => 1,
-			 'autoUpdate' => 1, 
-			 'expireDays' => 60,
-		)
-	),
-);
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fileName']['defaultToHTMLsuffixOnPrev'] = 1;
-$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fileName']['index']['.xml'] = array(
-	'keyValues' => array(
-		'type' => 1289377,
-	),
+    array(
+        'GETvar' => 'tx_podcast_display[action]',
+    ),
+    array(
+        'GETvar' => 'tx_podcast_display[podcast]',
+        'lookUpTable' => array(
+            'table' => 'tx_podcast_domain_model_podcast',
+            'id_field' => 'uid',
+            'alias_field' => 'title',
+            'addWhereClause' => ' AND NOT deleted AND NOT hidden',
+            'useUniqueCache' => 1,
+            'useUniqueCache_conf' => array(
+                'strtolower' => 1,
+                'spaceCharacter' => '-',
+            ),
+            'enable404forInvalidAlias' => 1,
+            'autoUpdate' => 1,
+            'expireDays' => 60,
+        ),
+    ),
+    array(
+        'GETvar' => 'tx_podcast_display[controller]',
+        'valueMap' => array(
+            'Podcast' => 'podcast'
+        ),
+        'noMatch' => 'bypass',
+    ),
 );
 
+//$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fileName']['defaultToHTMLsuffixOnPrev'] = 1;
+$TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['fileName']['index']['.xml'] = array(
+    'keyValues' => array(
+        'type' => 1289377,
+    ),
+);
 ?>
