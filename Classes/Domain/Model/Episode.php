58b1b76c03c176b6d519c6a9931adbd8e4317dfe
Sergej Junker <sj@plusb.de>
2016-10-26 10:34:44 +0200

    added check for emtpy filenames

--- Classes/Domain/Model/Episode.php
+++ Classes/Domain/Model/Episode.php
@@ -425,58 +425,82 @@ class Tx_Podcast_Domain_Model_Episode extends Tx_Extbase_DomainObject_AbstractEn
 	/**
 	 * Sets the mime
 	 *
-	 * @return void
+	 * @return string
 	 */
 	private function getFileMime($file) {
 		$mime = '';
 		$file = t3lib_div::getFileAbsFileName($file);
-		if ($fp = fopen($file, 'rb')) {
-			//This will set the Content-Type to the appropriate setting for the file 
-			$fileinfo = pathinfo($file);
-			$file_extension = strtolower($fileinfo['extension']);
-			switch($file_extension) {	   
-				case 'm4a': $mime='audio/x-m4a'; break;
-				case 'mp4': $mime='video/mp4'; break;
-				case 'm4v': $mime='video/x-m4v'; break;
-				case 'webm': $mime='video/webm'; break;
-				case 'mp3': $mime='audio/mpeg'; break;
-				case 'mov': $mime='video/quicktime'; break;
-				case 'pdf': $mime='application/pdf'; break;
-				case 'epub':$mime='document/x-epub'; break;
-				case 'mpg': $mime='video/mpeg'; break;
-				case 'avi': $mime='video/x-msvideo'; break;
-			}  
-		}
-		return $mime;
+        if ($file !== '') {
+            if ($fp = fopen($file, 'rb')) {
+                //This will set the Content-Type to the appropriate setting for the file
+                $fileinfo = pathinfo($file);
+                $file_extension = strtolower($fileinfo['extension']);
+                switch ($file_extension) {
+                    case 'm4a':
+                        $mime = 'audio/x-m4a';
+                        break;
+                    case 'mp4':
+                        $mime = 'video/mp4';
+                        break;
+                    case 'm4v':
+                        $mime = 'video/x-m4v';
+                        break;
+                    case 'webm':
+                        $mime = 'video/webm';
+                        break;
+                    case 'mp3':
+                        $mime = 'audio/mpeg';
+                        break;
+                    case 'mov':
+                        $mime = 'video/quicktime';
+                        break;
+                    case 'pdf':
+                        $mime = 'application/pdf';
+                        break;
+                    case 'epub':
+                        $mime = 'document/x-epub';
+                        break;
+                    case 'mpg':
+                        $mime = 'video/mpeg';
+                        break;
+                    case 'avi':
+                        $mime = 'video/x-msvideo';
+                        break;
+                }
+            }
+        }
+        return $mime;
 	}
 	
 	
 	/**
 	 * Sets the duration
 	 *
-	 * @param integer $duration
-	 * @return void
+	 * @param string $file
+	 * @return integer
 	 */
 	private function getFileDuration($file) {
 		$duration = 0;
 		$file = t3lib_div::getFileAbsFileName($file);
-		if ($fp = fopen($file, 'rb')) {
-			require_once('typo3conf/ext/podcast/Classes/Utilities/getid3/getid3.php');
+        if ($file !== '') {
+            if ($fp = fopen($file, 'rb')) {
+                require_once('typo3conf/ext/podcast/Classes/Utilities/getid3/getid3.php');
 
-			// Initialize getID3 engine
-			$getID3 = new getID3;
-			$getID3->option_md5_data        = true;
-			$getID3->option_md5_data_source = true;
-			$getID3->encoding               = 'UTF-8';
-			$getID3->analyze($file);
-			if (empty($getID3->info['error'])) {
-				// Init wrapper object
-				$result = array();
-				$result['playing_time']    = (isset($getID3->info['playtime_seconds']) ? $getID3->info['playtime_seconds'] : '');
-				$duration= round($result['playing_time']);
-				
-			}
-		}
+                // Initialize getID3 engine
+                $getID3 = new getID3;
+                $getID3->option_md5_data = true;
+                $getID3->option_md5_data_source = true;
+                $getID3->encoding = 'UTF-8';
+                $getID3->analyze($file);
+                if (empty($getID3->info['error'])) {
+                    // Init wrapper object
+                    $result = array();
+                    $result['playing_time'] = (isset($getID3->info['playtime_seconds']) ? $getID3->info['playtime_seconds'] : '');
+                    $duration = round($result['playing_time']);
+
+                }
+            }
+        }
 		return $duration;
 	}
 	
